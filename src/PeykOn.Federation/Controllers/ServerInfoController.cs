15cda5217c83f7c544490d23d382cdfdc71af16c
Poulad Ashrafpour <poulad.pld@gmail.com>
2019-01-05 14:08:18 -0500

    Add server discovery endpoint

---
+++ src/PeykOn.Federation/Controllers/ServerInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace PeykOn.Federation.Controllers
+{
+    [Route("_matrix/federation/v1/version")]
+    [ApiController]
+    public class ServerInfoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var response = new
+            {
+                server = new
+                {
+                    name = "peykon.herokuapp.com:443",
+                    version = "0.0.0-alpha1"
+                }
+            };
+
+            return Json(response, new JsonSerializerSettings {Formatting = Formatting.None});
+        }
+    }
+}
